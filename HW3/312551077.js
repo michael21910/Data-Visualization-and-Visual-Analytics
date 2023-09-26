b847f87ccabbf1caa5b100ae793df8aedbc475d4
michael21910 <faefae31910@gmail.com>
2023-09-26 19:19:55 +0800

    [ 2023/09/26 ] [ Drawing correlogram left ]

--- HW3/312551077.js
+++ HW3/312551077.js
@@ -9,16 +9,17 @@ fetch(dataPath)
         dataArray = contents.split("\n");
         dataArray = dataArray.map(row => row.split(","));
         dataArray = dataArray.map(row => row.map((value, index) => index === 0 ? value : parseFloat(value)));
-        const maleData = new DataObj(dataArray.filter(data => data[0] === "M"));
-        const femaleData = new DataObj(dataArray.filter(data => data[0] === "F"));
-        const infantData = new DataObj(dataArray.filter(data => data[0] === "I"));
-
-        
-        console.log("Correlation Matrix:");
-        console.log(maleData.CalcCorrelationMatrix());
-        console.log(femaleData.CalcCorrelationMatrix());
-        console.log(infantData.CalcCorrelationMatrix());
+        const maleData = new DataObj(dataArray.filter(data => data[0] === "M")).CalcCorrelationMatrix();
+        const femaleData = new DataObj(dataArray.filter(data => data[0] === "F")).CalcCorrelationMatrix();
+        const infantData = new DataObj(dataArray.filter(data => data[0] === "I")).CalcCorrelationMatrix();
+        DrawCorrelationMatrix(maleData, "male");
+        DrawCorrelationMatrix(femaleData, "female");
+        DrawCorrelationMatrix(infantData, "infant");
     })
     .catch(error => {
         console.error('Error reading the file:', error);
     });
+
+function DrawCorrelationMatrix(data, tagName) {
+    
+}
