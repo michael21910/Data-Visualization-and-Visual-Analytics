44298958f55b7d0e0688d4636f359bf0c3b47e07
michael21910 <410821305@gms.ndhu.edu.tw>
2023-10-11 21:33:30 +0800

    [ 2023/10/11 ] [ HW4 histogram finished, brush left ]

--- HW4/312551077.js
+++ HW4/312551077.js
@@ -27,10 +27,8 @@ d3.csv('http://vis.lab.djosix.com:2023/data/iris.csv').then((data) => {
         .attr('height', height + margin.top + margin.bottom)
         .append('g')
         .attr('transform', `translate(${margin.left * 2},${margin.top})`);
-
     // attributes for x and y axis
     const attributes = ['sepal length', 'sepal width', 'petal length', 'petal width'];
-
     for (let i = 0; i < attributes.length; i++) {
         for (let j = 0; j < attributes.length; j++) {
             const xValue = attributes[j];
@@ -39,45 +37,41 @@ d3.csv('http://vis.lab.djosix.com:2023/data/iris.csv').then((data) => {
                 .append('g')
                 .attr('class', 'subplot')
                 .attr('transform', `translate(${j * (subplotSize + padding)}, ${i * (subplotSize + padding)})`);
-            // add brush
-            /*
-            const brush = d3
-                .brush()
-                .extent([
-                    [0, 0],
-                    [subplotSize, subplotSize],
-                ])
-                .on('brush', updateBrushed);
-            subplotGroup.append('g').attr('class', 'brush').call(brush);
-            */
             // draw scatter plot and histogram
+            const innerPadding = 0.15;
+            const xExtent = [Math.floor(d3.min(data, (d) => d[xValue])) - innerPadding, Math.ceil(d3.max(data, (d) => d[xValue])) + innerPadding];
             if (xValue === yValue) {
                 // histogram
                 const binSize = 0.5;
-                const xExtent = [Math.floor(d3.min(data, (d) => d[xValue])), Math.ceil(d3.max(data, (d) => d[xValue]))];
-
+                // histogram X axis set up
                 const xHistogram = d3
                     .histogram()
                     .value((d) => d[xValue])
                     .domain(xExtent)
                     .thresholds(d3.range(Math.floor(xExtent[0]), Math.ceil(xExtent[1]) + binSize, binSize))(data);
-
+                // get maximum Y count
                 const maxYCount = d3.max(xHistogram, (d) => d.length);
-
+                // histogram Y axis set up
                 const yScaleHistogram = d3.scaleLinear().range([subplotSize, 0]);
                 yScaleHistogram.domain([0, maxYCount]);
-
+                // histogram X axis set up
                 const xScaleHistogram = d3.scaleLinear().range([0, subplotSize]);
                 xScaleHistogram.domain(xExtent);
-
+                // draw histogram X axis
                 subplotGroup
                     .append('g')
                     .attr('class', 'x-axis')
                     .attr('transform', `translate(0, ${subplotSize})`)
-                    .call(d3.axisBottom(xScaleHistogram).tickFormat(d3.format(".1f")));
-
-                subplotGroup.append('g').attr('class', 'y-axis').call(d3.axisLeft(yScaleHistogram).ticks(5));
-
+                    .call(d3.axisBottom(xScaleHistogram).ticks((xExtent[1] - xExtent[0]) / binSize).tickFormat((d) => {
+                        if (Number.isInteger(d)) return d;
+                        else return d.toFixed(1);
+                    }));
+                // draw histogram Y axis
+                subplotGroup.append('g').attr('class', 'y-axis').call(d3.axisLeft(yScaleHistogram).ticks(5).tickFormat((d) => {
+                    if (Number.isInteger(d)) return d;
+                    else return d.toFixed(1);
+                }));
+                // draw histogram bar
                 subplotGroup
                     .selectAll('.bar-x')
                     .data(xHistogram)
@@ -88,21 +82,18 @@ d3.csv('http://vis.lab.djosix.com:2023/data/iris.csv').then((data) => {
                     .attr('width', (d) => xScaleHistogram(d.x1) - xScaleHistogram(d.x0))
                     .attr('y', (d) => yScaleHistogram(d.length))
                     .attr('height', (d) => subplotSize - yScaleHistogram(d.length))
-                    .style('fill', 'black')
-                    .style('opacity', 0.7);
+                    .style('fill', 'lightblue')
+                    .style('opacity', 1)
+                    .style('stroke', 'white')
+                    .style('stroke-width', 0.5);
             }
             else {
                 // scatter plot X Y axis
-                const innerPadding = 0.2;
-                const xExtent = d3.extent(data, (d) => d[xValue]);
                 const yExtent = d3.extent(data, (d) => d[yValue]);
-                xExtent[0] -= innerPadding;
-                xExtent[1] += innerPadding;
                 yExtent[0] -= innerPadding;
                 yExtent[1] += innerPadding;
                 const x = d3.scaleLinear().domain(xExtent).range([0, subplotSize]);
                 const y = d3.scaleLinear().domain(yExtent).range([subplotSize, 0]);
-
                 // scatter plot
                 subplotGroup
                     .selectAll('.dot')
@@ -118,38 +109,23 @@ d3.csv('http://vis.lab.djosix.com:2023/data/iris.csv').then((data) => {
                         else if (d.class === "Iris-versicolor") return "green";
                         else return "blue";
                     })
-                    .style('opacity', 0.7);
+                    .style('opacity', 1);
                 // draw X axis
                 subplotGroup
-                  .append('g')
-                  .attr('class', 'x-axis')
-                  .attr('transform', `translate(0, ${subplotSize})`)
-                  .style('font-size', '10px')
-                  .call(d3.axisBottom(x).ticks(6));
+                    .append('g')
+                    .attr('class', 'x-axis')
+                    .attr('transform', `translate(0, ${subplotSize})`)
+                    .style('font-size', '10px')
+                    .call(d3.axisBottom(x).ticks((xExtent[1] - xExtent[0]) / 0.5).tickFormat((d) => {
+                        if (Number.isInteger(d)) return d;
+                        else return d.toFixed(1);
+                    }));
                 // draw Y axis
-                subplotGroup.append('g').attr('class', 'y-axis').call(d3.axisLeft(y).ticks(6));
+                subplotGroup.append('g').attr('class', 'y-axis').call(d3.axisLeft(y).ticks(6).tickFormat((d) => {
+                    if (Number.isInteger(d)) return d;
+                    else return d.toFixed(1);
+                }));
             }
         }
-    }
-    // initialize brush data
-    let brushedData = [];
-    // update matrix cells
-    function updateBrushed() {
-        brushedData = [];
-        svg.selectAll('.brush .selection').each(function () {
-            const extent = d3.select(this).datum();
-            brushedData = brushedData.concat(
-                data.filter(
-                    (d) =>
-                        d[currentX] >= extent[0][0] &&
-                        d[currentX] <= extent[1][0] &&
-                        d[currentY] >= extent[0][1] &&
-                        d[currentY] <= extent[1][1]
-                )
-            );
-        });
-        svg
-            .selectAll('.dot')
-            .style('fill', (d) => (brushedData.includes(d) ? color(d.species) : 'gray'));
     }
 });
