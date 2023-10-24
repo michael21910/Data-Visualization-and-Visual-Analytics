f31e1346536f0bd64e9fd4d2a033d63d6304a62a
michael21910 <410821305@gms.ndhu.edu.tw>
2023-10-24 12:01:20 +0800

    [ 2023/10/24 ] [ HW6 static finished ]

--- HW6/312551077.js
+++ HW6/312551077.js
@@ -1,9 +1,9 @@
 // http://vis.lab.djosix.com:2023/data/ma_lga_12345.csv
 
 // constants for svg
-const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+const margin = { top: 20, right: 20, bottom: 40, left: 80 };
 const width = 1200 - margin.left - margin.right
-const height = 800 - margin.top - margin.bottom;
+const height = 600 - margin.top - margin.bottom;
 
 // create svg
 const svg = d3.select('#themeriver')
@@ -12,6 +12,60 @@ const svg = d3.select('#themeriver')
     .attr('height', height + margin.top + margin.bottom)
     .append('g')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+function PreprocessData(data) {
+    // sort data by saledate in ascending order
+    data.sort((a, b) => a.saledate - b.saledate);
+    // list to store the keys of the preprocessed data
+    var keys = ['saledate'];
+    // get all keys of the preprocessed data
+    for (let i = 0; i < data.length; i++) {
+        const newAttr = data[i].type + '-' + data[i].bedrooms;
+        if (keys.indexOf(newAttr) === -1) {
+            keys.push(newAttr);
+        }
+    }
+    // sort the keys
+    keys.sort();
+    // create object base on keys
+    var preprocessedData = [];
+    var currentDate = new Date(1970, 1, 1);
+    var obj = {};
+    for (let i = 0; i < data.length; i++) {
+        // if the 'currentDate' is not the same as the current date, create a new object
+        if (currentDate.getTime() !== data[i].saledate.getTime()) {
+            // refresh the 'currentDate'
+            currentDate = data[i].saledate;
+            // prevent adding the first empty object
+            if (Object.keys(obj).length !== 0) {
+                preprocessedData.push(obj);
+            }
+            // clear the object
+            obj = {};
+            // set the saledate to the object
+            obj['saledate'] = data[i].saledate;
+        }
+        // add the MA to the object, with the custom key
+        obj[data[i].type + '-' + data[i].bedrooms] = data[i].MA;
+    }
+    // padding 0 to the object with missing key
+    for (let i = 0; i < preprocessedData.length; i++) {
+        for (let j = 0; j < keys.length; j++) {
+            if (preprocessedData[i][keys[j]] === undefined) {
+                preprocessedData[i][keys[j]] = 0;
+            }
+        }
+    }
+    // change the order of each element in object according to the keys
+    for (let i = 0; i < preprocessedData.length; i++) {
+        var temp = {};
+        for (let j = 0; j < keys.length; j++) {
+            temp[keys[j]] = preprocessedData[i][keys[j]];
+        }
+        preprocessedData[i] = temp;
+    }
+    return [keys, preprocessedData];
+}
 
 // read data
 d3.csv('./ma_lga_12345.csv').then((data) => {
@@ -26,7 +80,11 @@ d3.csv('./ma_lga_12345.csv').then((data) => {
         d.type = d.type;
         d.bedrooms = +d.bedrooms;
     });
-    console.log(data);
+
+    // get the return data from the function "PreprocessData"
+    const ReturnOfPreprocessData = PreprocessData(data);
+    const keys = ReturnOfPreprocessData[0];
+    data = ReturnOfPreprocessData[1];
 
     // X axis
     const x = d3.scaleTime()
@@ -41,19 +99,19 @@ d3.csv('./ma_lga_12345.csv').then((data) => {
 
     // X axis text
     svg.append('text')
-        .attr('transform', 'translate(' + (width / 2) + ' ,' + (height - 5) + ')')
+        .attr('transform', 'translate(' + (width / 2) + ' ,' + (height + 40) + ')')
         .attr('text-anchor', 'middle')
         .text('Time');
 
     // Y axis
     const y = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.MA)])
+        .domain([-2500000, 2500000])
         .range([height, 0]);
 
     // Y axis label
     svg.append('g')
         .attr('transform', 'translate(0, 0)')
-        .call(d3.axisRight(y))
+        .call(d3.axisLeft(y))
         .attr('class', 'y-axis');
 
     // Y axis text
@@ -66,24 +124,35 @@ d3.csv('./ma_lga_12345.csv').then((data) => {
 
     // set color
     const color = d3.scaleOrdinal()
-        .domain(['house', 'unit'])
-        .range(['#e41a1c', '#377eb8']);
+        .domain(keys)
+        .range(d3.schemeCategory10);
 
     // stack data
-    /* TODO */
-
-    // area generator
-    /* TODO */
+    const stackedData = d3.stack()
+        .offset(d3.stackOffsetSilhouette)
+        .keys(keys.filter(function (key) {
+            return key !== 'saledate';
+        }))(data);
 
     // draw themeriver
-    /* TODO */
+    svg.selectAll('mylayers')
+        .data(stackedData)
+        .join('path')
+        .style('fill', d => color(d.key))
+        .attr('d', d3.area()
+            .x(d => x(d.data.saledate))
+            .y0(d => y(d[0]))
+            .y1(d => y(d[1]))
+        );
 
     // add legend to svg
     const legend = svg.append('g')
         .attr('transform', 'translate(' + (width - 100) + ', 20)');
 
-    legend.selectAll("rect")
-        .data(['house', 'unit'])
+    legend.selectAll('rect')
+        .data(keys.filter(function (key) {
+            return key !== 'saledate';
+        }).reverse())
         .enter()
         .append('rect')
         .attr('x', 0)
@@ -92,8 +161,10 @@ d3.csv('./ma_lga_12345.csv').then((data) => {
         .attr('height', 18)
         .style('fill', d => color(d));
 
-    legend.selectAll("text")
-        .data(['house', 'unit'])
+    legend.selectAll('text')
+        .data(keys.filter(function (key) {
+            return key !== 'saledate';
+        }).reverse())
         .enter()
         .append('text')
         .attr('x', 25)
